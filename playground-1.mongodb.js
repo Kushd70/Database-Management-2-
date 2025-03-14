d9ce2c0b87a7c72f821876774d49e247b09fcdde
Kushd70 <abeykoonkushan@gmail.com>
2025-03-14 12:34:41 +0530

    c

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -7,9 +7,9 @@ db.createCollection('users'); // no fixed attributes (schema less)
 
 // collection with flexible schema
 use('shop');
-db.createCOllection('categories', 
+db.createCollection('categories', 
     {
-        validators:{
+        validator:{
             $jsonSchema:{
                 bsonType:"object",
                 required:['code', 'name'],
@@ -26,3 +26,28 @@ db.createCOllection('categories',
             }
         }}
 );
+
+// Insert data
+// Inserting only one document
+let user = {
+    "name":"Jhohn Doe",
+    "email": "jhone@gmail.com",
+    "city":"kandy",
+    "type":"admin"
+};
+use('shop');
+db.getCollection('users').insertOne(user);
+
+// Inserting multiple documents at once
+let user_list=[
+    {"name":"Sam Silva", "city":"Colombo","type": "customer", "phone":"077777"},
+    {"name":"Angelo", "city":"Kandy", "type":"customer", "age":33},
+    {"name":"Ben", "city":"Kegalle"}
+];
+use('shop');
+db.getCollection('users').insertMany(user_list);
+
+
+// Selecting all the data from a collection
+use('shop');
+db.getCollection('users').find();

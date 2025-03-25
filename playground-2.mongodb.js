63fe489c86d6bcb53e4ed316f0430d79cbe6ba2a
Kushd70 <abeykoonkushan@gmail.com>
2025-03-25 10:01:38 +0530

    question 5

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -84,3 +84,6 @@ use('supermarket');
 db.getCollection('items').find({$and : [{price :{$gt : 1000}, stock : {$gt : 0}}]});
 
 
+// 5. Select items with supplier name as "Abc company".......
+use('supermarket');
+db.getCollection('')
